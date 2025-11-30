7872271f95c71ef9ea958b8a45f2d6ab401d84e0
kiwi-0506 <wu-qw23@mails.tsinghua.edu.cn>
2025-11-30 23:04:01 +0800

    update dependencies and example - update rsbullet to 0.3.3 - edit sim_only_physics_franka example to just load franka robot

--- examples/sim_only_physics_franka.rs
+++ examples/sim_only_physics_franka.rs
@@ -1,6 +1,5 @@
 use anyhow::Result;
 use franka_rust::FrankaEmika;
-use libjaka::JakaMini2;
 use robot_behavior::behavior::*;
 use rsbullet::{Mode, RsBullet};
 use std::{f64::consts::FRAC_PI_4, thread::sleep, time::Duration};
@@ -12,12 +11,7 @@ fn main() -> Result<()> {
         .set_gravity([0., 0., -9.81])?
         .set_step_time(Duration::from_secs_f64(1. / 240.))?;
 
-    let mut robot_1 = physics
-        .robot_builder::<JakaMini2>("robot_1")
-        .base([0.0, 0.2, 0.0])
-        .base_fixed(true)
-        .load()?;
-    let mut robot_2 = physics
+    let mut robot = physics
         .robot_builder::<FrankaEmika>("robot_2")
         .base([0.0, -0.2, 0.0])
         .base_fixed(true)
@@ -26,11 +20,7 @@ fn main() -> Result<()> {
     for _ in 0..100_000 {
         physics.step()?;
     }
-    robot_1
-        .with_velocity(&[5.; 6])
-        .with_acceleration(&[2.; 6])
-        .move_joint(&[0.; 6])?;
-    robot_2.move_joint(&[FRAC_PI_4; 7])?;
+    robot.move_joint(&[FRAC_PI_4; 7])?;
 
     loop {
         physics.step()?;
